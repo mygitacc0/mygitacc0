ea6c5b4d23c25fc4fd7faa54076a2682d5795af8
Andrii Tymchenko <timchenko2@gmail.com>
2019-12-14 13:21:35 +0200

    [Start]Added abstract factory pattern

--- start.cpp
+++ start.cpp
@@ -1,16 +1,75 @@
 #include <iostream>
+#include <string>
+#include <list>
 #include <vector>
-
-class Inf(){
-	public:
+#include <algorithm>
+using namespace std;
+class Infantryman
+{
+public:
 	virtual void info() = 0;
 };
-class RInf():public Inf{
-	public:
-	void info(){std::cout<<"RInf";}
+class RInfantryman : public Infantryman
+{
+public:
+	void info(){cout<<"RInfantryman"<<endl;}
 };
+class CInfantryman : public Infantryman
+{
+public:
+	void info(){cout<<"CInfantryman"<<endl;}
+};
+class Factory
+{
+public:
+	virtual  Infantryman* createInfantryman() = 0;
+};
+class RFactory : public Factory
+{
+public:
+	 Infantryman* createInfantryman()
+	{
+		return new RInfantryman();
+	}
+};
+class CFactory : public Factory
+{
+public:
+	 Infantryman* createInfantryman(){return new CInfantryman();}
+};
+class Army
+{
+public:
+	vector <Infantryman*> vi;
+	void info()
+	{
+		typedef vector<Infantryman*>::iterator InfantrtmanVectorIt;
+		for(InfantrtmanVectorIt it = vi.begin();it!=vi.end();++it)
+		{
+			(*it)->info();
+		}
+	}
+};
+class Game
+{
+public:
+	Army* createArmy(Factory& f)
+	{
+       Army* p = new Army();
+	   p->vi.push_back(f.createInfantryman());
+	   return p;
+	}
+};
+
 int main(){
-    std::cout<<"hi";
+     Game game;
+   RFactory rf;
+   CFactory cf;
+   Army* ra = game.createArmy(rf);
+   Army* ca = game.createArmy(cf);
+   ra->info();
+   ca->info();
+
 	return 1;
 }
 
