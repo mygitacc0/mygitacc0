92d96a6488f0bf3264135b6a2f6d1929d1f3e3ee
Andrii Tymchenko <timchenko2@gmail.com>
2019-12-14 13:04:53 +0200

    [Start] Added class Inf and RInf

--- start.cpp
+++ start.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <vector>
 
-
-
+class Inf(){
+	public:
+	virtual void info() = 0;
+};
+class RInf():public Inf{
+	public:
+	void info(){std::cout<<"RInf";}
+};
 int main(){
     std::cout<<"hi";
 	return 1;
